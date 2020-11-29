68f5068703be613421384b7f19edf0d76daefdc2
unknown <yummyfunnybunny13@gmail.com>
2020-11-28 23:55:40 -0500

    refactored request handlers

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
+//  REQUIRE MODULES
 const fs = require('fs');
 const express = require('express');
 
-const app = express();  // common practice to create a variable called "app" that is set to the newly requirec express
+// common practice to create a variable called "app" that is set to the newly requirec express
+const app = express();  
 
 // MIDDLEWARE - function that can modifer the incoming request data (stands between the request and the response)
 app.use(express.json());
-
-// GET ROOT HANDLER
-// -----------------------------------------
-app.get('/', (request, response) => {
-  // set the response that is 'sent' back from the server
-  // also, you can use the 'status' method to set the status type of the response (in this case it is '200' for 'success')
-  // response.status(200).send('Hello from the server side!');
-
-  // you can also send json requests back from the server like this:
-  // the 'json' method automatically sets the content type to json, so we do not need to manually set that up
-  // like we did previously when we were not using express methods
-  response.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
-});
-
-// BASIC POST REQUEST HANDLER
-// -----------------------------------------
-app.post('/', (request, response) => {
-  response.send('You can post to this endpoint...');
-});
 
 // save the data we want into a variable in a synchronous way (since it does not matter in top-level code)
 // we will parse this data into a JSON file right away
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-// GET ROUTE HANDLER
-// -----------------------------------------
-app.get('/api/v1/tours', (req,res) => {
+// FUNCTION: GET ALL TOURS
+const getAllTours = (req,res) => {
   // useful to list the 'status' even if its not needed
   // we will be using the 'JSend' response format with 'enveloping':
   res.status(200).json({
@@ -47,28 +29,42 @@ app.get('/api/v1/tours', (req,res) => {
       // "tours: x", because enveloping name we want is different from the variable with the saved data that we want
     }
   })
-});
+};
 
-// GET REQUEST WITH ID HANDLER
-// -----------------------------------------
-// notice the ':id' at the end of the url, that syntax will check for a variable, in this case, the var "id"
-app.get('/api/v1/tours/:id', (req,res) => {
+// FUNCTION: GET TOUR BY ID
+const getTour = (req,res) => {
   // variables in the URL are called 'parameters', and they are stored inside 'req.params'
+  console.log(req.params);  // { id: '5' } - this is an object
+
+  // when you multiply a string-number by a real number, it will turn into a real number
+  const id = req.params.id * 1; 
   
-  console.log(req.params);
+  // iterate through the 'tours' array and return the value of the first tour whose id matches the searched-for id
+  const tour = tours.find(el => el.id === id);
+  
+  // check if the id being searched for is valid
+  // if (id > tours.length) {
+  // OR
+  // check if the 'tour' variable that came back is 'undefined' from the 'find' method above
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID'
+    });
+  }
+
+  // send the response with the correct tour data back to the client
   res.status(200).json({
     status: 'success',
-    // results: tours.length,
-    // data: {
-    //   tours
-    // }
+    results: tours.length,
+    data: {
+      tour
+    }
   })
-});
+}
 
-
-// POST ROUTE HANDLER
-// -----------------------------------------
-app.post('/api/v1/tours', (req, res) => {
+// FUNCTION: CREATE TOUR
+const createTour = (req, res) => {
   // we do 'req.body' because body is the property that is going to be available on the request...
   // console.log(req.body);
 
@@ -98,14 +94,75 @@ app.post('/api/v1/tours', (req, res) => {
 
   // must have a response of some kind to complete the request/response cycle
   // res.send("Done");
-});
+};
 
+// FUNCTION: UPDATE TOUR
+const updateTour = (req, res) => {
+  // invalid ID search handling
+  if ((req.params.id * 1) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID'
+    });
+  }
 
-// create a variable for the port
-const port = 3000;
+  // testing the patch
+  // const id = req.params.id * 1; 
+  // const tour = tours.find(el => el.id === id);
+  // tour.duration = req.body.duration;
+
+  // valid ID search handling
+  res.status(200).json({
+    status: 'success',
+    data: {
+      // tour: "<Updated tour here...>"
+      tour
+    }
+  })
+};
+
+// FUNCTION: DELETE TOUR
+const deleteTour = (req, res) => {
+  if ((req.params.id * 1) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID'
+    });
+  }
+
+  // valid ID search handling
+  // '204' = 'success: no content', which is what we want for a delete request 
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+};
+
+// ALL ROUTES
+// app.get('/api/v1/tours', getAllTours);
+// notice the ':id' at the end of the url, that syntax will check for a variable, in this case, the var "id"
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 // SET LISTENING HANDLER
 // -----------------------------------------
+// create a variable for the port
+const port = 3000;
+
+// set the listen handler using the 'port' variable
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
