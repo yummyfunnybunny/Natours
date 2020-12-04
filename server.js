7b5883f3b4734f2177b0329558b00c995be6eb95
unknown <yummyfunnybunny13@gmail.com>
2020-12-03 19:54:27 -0500

    finished mongoDB section and set up Mongoose for use

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 // REQURE MODULES
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({
   path: './config.env',
 }); // syntax for requiring the config.env file
-
 console.log(`current environment: ${app.get('env')}`);
+
+// connect to MongoDB Atlas
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB, {
+    // these are just to handle deprecation warnings, so copy/paste this in other projects with mongooese
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 // create a variable for the port
 const port = process.env.PORT || 3000;
