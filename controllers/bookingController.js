b6d3202f25b192808969ba3166e0aa086272f44b
unknown <yummyfunnybunny13@gmail.com>
2021-01-18 14:20:53 -0500

    updated stripe functions for newer version

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -31,19 +31,38 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
+    mode: 'payment',
     // 3.B) info about the item that is about to be purchased
     line_items: [
       {
-        name: `${tour.name} Tour`,
-        description: tour.summary,
-        images: [
-          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
-        ],
-        amount: tour.price * 100,
-        currency: 'usd',
         quantity: 1,
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
+          },
+        },
       },
     ],
+    // line_items: [
+    //   {
+    //     name: `${tour.name} Tour`,
+    //     description: tour.summary,
+    //     images: [
+    //       `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+    //     ],
+    //     amount: tour.price * 100,
+    //     currency: 'usd',
+    //     quantity: 1,
+    //   },
+    // ],
   });
 
   // 4) Create session as response
@@ -68,7 +87,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  // const price = session.display_items[0].amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
